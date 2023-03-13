456c4599560429fa1e67d9e5ef56bf6d891a0357
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2023-03-13 20:29:24 +0200

    init data init tailwind css

---
+++ src/components/data.js
@@ -0,0 +1,11 @@
+
+
+export const questions = {
+  "thermbocytopeina": [{ "Other causes for thrombocytopenia Other causes for thrombocytopenia Other causes for thrombocytopenia": 2 }, { "a choise": 0 }, { chose: 1 }],
+  "Timing of platelet count fall": [{ "some choise": 0  }, { "a choise": 1 }, { chose: 2 }],
+  "Thrombosis or other sequelae": [{ "some choise": 0 }, { "a choise": 1 }, { chose: 2 }],
+  "Other causes for thrombocytopenia": [{ "some choise": 0 }, { "a choise": 1 }, { chose: 2 }],
+
+};
+
+
