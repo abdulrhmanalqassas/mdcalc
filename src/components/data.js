1d1b41eecdea35381b0b4a9932c491d1f0cddeed
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2023-03-18 02:02:31 +0200

    update questions form

--- src/components/data.js
+++ src/components/data.js
@@ -1,23 +1,31 @@
 export const questions = {
-  "Thrombocytopenia": [
+  Thrombocytopenia: [
     { "Platelet count fall >50% AND platelet nadir ≥20": 2 },
     { "Platelet count fall 30–50% OR platelet nadir 10–19": 1 },
     { "Platelet count fall <30% OR platelet nadir <10": 0 },
   ],
   "Timing of platelet count fall": [
-    { "Clear onset between days 5–10 OR platelet fall ≤1 day (prior heparin exposure within 30 days)": 2 },
-    { "Consistent with days 5–10 fall, but not clear; onset after day 10 OR fall ≤1 day (prior heparin exposure 30–100 days ago)": 1 },
+    {
+      "Clear onset between days 5–10 OR platelet fall ≤1 day (prior heparin exposure within 30 days)": 2,
+    },
+    {
+      "Consistent with days 5–10 fall, but not clear; onset after day 10 OR fall ≤1 day (prior heparin exposure 30–100 days ago)": 1,
+    },
     { "Platelet count fall <4 days without recent exposure": 0 },
   ],
   "Thrombosis or other sequelae": [
-    { "New thrombosis OR skin necrosis; acute systemic reaction post-IV heparin bolus": 2 },
-    { "Progressive OR recurrent thrombosis; non-necrotizing skin lesions; suspected thrombosis (not proven)": 1 },
-    { "None": 0 },
+    {
+      "New thrombosis OR skin necrosis; acute systemic reaction post-IV heparin bolus": 2,
+    },
+    {
+      "Progressive OR recurrent thrombosis; non-necrotizing skin lesions; suspected thrombosis (not proven)": 1,
+    },
+    { None: 0 },
   ],
   "Other causes for thrombocytopenia": [
     { "None apparent": 2 },
-    { "Possible": 1 },
-    { "Definite": 0 },
+    { Possible: 1 },
+    { Definite: 0 },
   ],
 };
 //////////////////
@@ -28,108 +36,66 @@ export const ISTHquestions = {
     { "50 to <100": 1 },
     { "<50": 2 },
   ],
-  "Elevated levels of a fibrinrelated marker (e.g. Ddimer, fibrin degradation products": [
-    { "No increase": 0},
-    { "Moderateincrease": 2 },
-    { "Severe increase": 3 },
-  ],
-  "Prolonged PT, seconds": [
-    { "<3": 0 },
-    { "3 to <6": 1 },
-    { "≥6": 2 },
-  ],
-  "Fibrinogen level, g/L": [
-    { "≥1": 0 },
-    { "<1": 1 },
-    
-  ],
+  "Elevated levels of a fibrinrelated marker (e.g. Ddimer, fibrin degradation products":
+    [{ "No increase": 0 }, { Moderateincrease: 2 }, { "Severe increase": 3 }],
+  "Prolonged PT, seconds": [{ "<3": 0 }, { "3 to <6": 1 }, { "≥6": 2 }],
+  "Fibrinogen level, g/L": [{ "≥1": 0 }, { "<1": 1 }],
 };
 
 ////////////////
 export const SIRSquestions = {
-  "Temp >38°C (100.4°F) or <36°C (96.8°F)": [
-    { "YES": 1 },
-    { "NO": 0 },
+  "Temp >38°C (100.4°F) or <36°C (96.8°F)": [{ YES: 1 }, { NO: 0 }],
+  "Heart rate >90": [{ YES: 1 }, { NO: 0 }],
+  "Respiratory rate >20 or PaCO₂ <32 mm Hg": [{ YES: 1 }, { NO: 0 }],
+  "WBC >12,000/mm³, <4,000/mm³, or >10% bands": [{ YES: 1 }, { NO: 0 }],
+  "Suspected or present source of infection": [{ YES: 1 }, { NO: 0 }],
+  "Lactic acidosis, SBP <90 or SBP drop ≥40 mm Hg of normal": [
+    { YES: 1 },
+    { NO: 0 },
   ],
-  "Heart rate >90": [
-    { "YES": 1 },
-    { "NO": 0 },
+  "Severe sepsis with hypotension, despite adequate fluid resuscitation": [
+    { YES: 1 },
+    { NO: 0 },
   ],
-  "Respiratory rate >20 or PaCO₂ <32 mm Hg": [
-    { "YES": 1 },
-    { "NO": 0 },
-  ],
-  "WBC >12,000/mm³, <4,000/mm³, or >10% bands":[
-    { "YES": 1 },
-    { "NO": 0 },
-  ],
-  "Suspected or present source of infection":[
-    { "YES": 1 },
-    { "NO": 0 },
-  ],
-  "Lactic acidosis, SBP <90 or SBP drop ≥40 mm Hg of normal":[
-    { "YES": 1 },
-    { "NO": 0 },
-  ],
-"Severe sepsis with hypotension, despite adequate fluid resuscitation":[
-  { "YES": 1 },
-  { "NO": 0 },
-]
 };
 
-// 
-export const khoronaQuestions ={
+//
+export const khoronaQuestions = {
   "Cancer type": [
-    { "Stomach": 2 },
-    { "Pancreas": 2 },
-    { "Lung": 1 },
-    { "Lymphoma": 1 },
-    { "Gynecologic": 1 },
-    { "Bladder": 1 },
-    { "Testicular": 1 },
-    { "Other": 0 },
+    { Stomach: 2 },
+    { Pancreas: 2 },
+    { Lung: 1 },
+    { Lymphoma: 1 },
+    { Gynecologic: 1 },
+    { Bladder: 1 },
+    { Testicular: 1 },
+    { Other: 0 },
   ],
-  "Pre-chemotherapy platelet count ≥350x10⁹/L":[
-    { "YES": 1 },
-    { "NO": 0 },
+  "Pre-chemotherapy platelet count ≥350x10⁹/L": [{ YES: 1 }, { NO: 0 }],
+  "Hemoglobin level <10 g/dL or using RBC growth factors": [
+    { YES: 1 },
+    { NO: 0 },
   ],
-  "Hemoglobin level <10 g/dL or using RBC growth factors":[
-    { "YES": 1 },
-    { "NO": 0 },
-  ],
-  "Pre-chemotherapy leukocyte count >11x10⁹/L":[
-    { "YES": 1 },
-    { "NO": 0 },
-  ],
+  "Pre-chemotherapy leukocyte count >11x10⁹/L": [{ YES: 1 }, { NO: 0 }],
 };
 //////////////////
 export const HASquestions = {
-  "Temp >38°C (100.4°F) or <36°C (96.8°F)": [
-    { "YES": 1 },
-    { "NO": 0 },
+  "Hypertension Uncontrolled, >160 mmHg systolic": [{ YES: 1 }, { NO: 0 }],
+  "Renal disease Dialysis, transplant, Cr >2.26 mg/dL or >200 µmol/L": [
+    { YES: 1 },
+    { NO: 0 },
   ],
-  "Heart rate >90": [
-    { "YES": 1 },
-    { "NO": 0 },
+  "Liver disease Cirrhosis or bilirubin >2x normal with AST/ALT/AP >3x normal":
+    [{ YES: 1 }, { NO: 0 }],
+  "Stroke history": [{ YES: 1 }, { NO: 0 }],
+  "Prior major bleeding or predisposition to bleeding": [{ YES: 1 }, { NO: 0 }],
+  "Labile INR Unstable/high INRs, time in therapeutic range <60%": [
+    { YES: 1 },
+    { NO: 0 },
   ],
-  "Respiratory rate >20 or PaCO₂ <32 mm Hg": [
-    { "YES": 1 },
-    { "NO": 0 },
+  "Age >65": [{ YES: 1 }, { NO: 0 }],
+  "Medication usage predisposing to bleeding Aspirin, clopidogrel, NSAIDs": [
+    { YES: 1 },
+    { NO: 0 },
   ],
-  "WBC >12,000/mm³, <4,000/mm³, or >10% bands":[
-    { "YES": 1 },
-    { "NO": 0 },
-  ],
-  "Suspected or present source of infection":[
-    { "YES": 1 },
-    { "NO": 0 },
-  ],
-  "Lactic acidosis, SBP <90 or SBP drop ≥40 mm Hg of normal":[
-    { "YES": 1 },
-    { "NO": 0 },
-  ],
-"Severe sepsis with hypotension, despite adequate fluid resuscitation":[
-  { "YES": 1 },
-  { "NO": 0 },
-]
 };
