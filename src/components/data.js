7135aca50bdf4d985237dfaa54557360ab0c4977
el_qassas <a.alqssas@penta-b.com>
2023-04-07 23:02:29 +0200

    jjj

--- src/components/data.js
+++ src/components/data.js
@@ -44,23 +44,23 @@ export const ISTHquestions = {
 
 ////////////////
 export const SIRSquestions = {
-  "Temp >38°C (100.4°F) or <36°C (96.8°F)": [{ YES: 1 }, { "no": "0" }],
-  "Heart rate >90": [{ YES: 1 }, { "no": "0" }],
-  "Respiratory rate >20 or PaCO₂ <32 mm Hg": [{ YES: 1 }, { "no": "0" }],
-  "WBC >12,000/mm³, <4,000/mm³, or >10% bands": [{ YES: 1 }, { "no": "0" }],
+  "Temp >38°C (100.4°F) or <36°C (96.8°F)": [{ YES: 1 }, { "no": 0 }],
+  "Heart rate >90": [{ YES: 1 }, { "no": 0 }],
+  "Respiratory rate >20 or PaCO₂ <32 mm Hg": [{ YES: 1 }, { "no": 0 }],
+  "WBC >12,000/mm³, <4,000/mm³, or >10% bands": [{ YES: 1 }, { "no": 0 }],
 
-  "Suspected or present source of infection": [{ YES: 0 }, { "no": "0" }],
+  "Suspected or present source of infection": [{ YES: 0 }, { "no": 0 }],
   "Lactic acidosis, SBP <90 or SBP drop ≥40 mm Hg of normal": [
     { YES: 0},
-    { "no": "0" },
+    { "no": 0 },
   ],
   "Severe sepsis with hypotension, despite adequate fluid resuscitation": [
     { YES: 0 },
-    { "no": "0" },
+    { "no": 0 },
   ],
   "Evidence of ≥2 organs failing": [
     { YES: 0 },
-    { "no": "0" },
+    { "no": 0 },
   ],
 };
 
@@ -76,18 +76,18 @@ export const khoronaQuestions = {
     { Testicular: 1 },
     { Other: 0 },
   ],
-  "Pre-chemotherapy platelet count ≥350x10⁹/L": [{ YES: 1 }, { "no": "0" }],
+  "Pre-chemotherapy platelet count ≥350x10⁹/L": [{ YES: 1 }, { "no": 0 }],
   
   "Hemoglobin level <10 g/dL or using RBC growth factors": [
     { YES: 1 },
-    { "no": "0" },
+    { "no": 0 },
   ],
-  "Pre-chemotherapy leukocyte count >11x10⁹/L": [{ YES: 1 }, { "no": "0" }]
+  "Pre-chemotherapy leukocyte count >11x10⁹/L": [{ YES: 1 }, { "no": 0 }]
   , "BMI ≥35 kg/m^2 ": [{ YES: 1 }, { "no": 0 }],
 };
 //////////////////
 export const HASquestions = {
-  "Hypertension Uncontrolled, >160 mmHg systolic": [{ YES: 1 }, { "no": "0" }],
+  "Hypertension Uncontrolled, >160 mmHg systolic": [{ YES: 1 }, { "no": 0 }],
   "Renal disease Dialysis, transplant, Cr >2.26 mg/dL or >200 µmol/L": [
     { YES: 1 },
     { "no": 0 },
@@ -95,7 +95,7 @@ export const HASquestions = {
   "Liver disease Cirrhosis or bilirubin >2x normal with AST/ALT/AP >3x normal":
     [{ YES: 1 }, { "no": 0 }],
   "Stroke history": [{ YES: 1 }, { "no": 0 }],
-  "Prior major bleeding or predisposition to bleeding": [{ YES: 1 }, { "no": "0" }],
+  "Prior major bleeding or predisposition to bleeding": [{ YES: 1 }, { "no": 0 }],
   "Labile INR Unstable/high INRs, time in therapeutic range <60%": [
     { YES: 1 },
     { "no": 0 },
@@ -112,7 +112,7 @@ export const HASquestions = {
 };
 
 export const last = {
-  "Hypertension Uncontrolled, >160 mmHg systolic": [{ YES: 1 }, { "no": "0" }],
+  "Hypertension Uncontrolled, >160 mmHg systolic": [{ YES: 1 }, { "no": 0 }],
   "Renal disease Dialysis, transplant, Cr >2.26 mg/dL or >200 µmol/L": [
     { YES: 1 },
     { "no": 0 },
@@ -120,7 +120,7 @@ export const last = {
   "Liver disease Cirrhosis or bilirubin >2x normal with AST/ALT/AP >3x normal":
     [{ YES: 1 }, { "no": 0 }],
   "Stroke history": [{ YES: 1 }, { "no": 0 }],
-  "Prior major bleeding or predisposition to bleeding": [{ YES: 1 }, { "no": "0" }],
+  "Prior major bleeding or predisposition to bleeding": [{ YES: 1 }, { "no": 0 }],
   "Labile INR Unstable/high INRs, time in therapeutic range <60%": [
     { YES: 1 },
     { "no": 0 },
